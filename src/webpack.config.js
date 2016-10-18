36b45affe4cbabb24be67d3fd5246ccdc528b536
Matt Gadd <mgadd@sitepen.com>
2016-10-18 14:46:34 +0100

    add test dirs

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,13 +6,18 @@ const path = require('path');
 const basePath = process.cwd();
 
 module.exports = {
-	entry: [ path.join(basePath, 'src/main.ts') ],
+	entry: {
+		'src/main': [ path.join(basePath, 'src/main.ts') ],
+		'tests/unit/all': [ path.join(basePath, 'tests/unit/all.ts') ],
+		'tests/functional/all': [ path.join(basePath, 'tests/functional/all.ts') ]
+	},
 	devtool: 'source-map',
 	resolve: {
 		root: [ basePath ],
 		extensions: ['', '.ts', '.tsx', '.js'],
 		alias: {
-			rxjs: '@reactivex/rxjs/dist/amd'
+			rxjs: '@reactivex/rxjs/dist/amd',
+			intern: path.join(__dirname, 'intern')
 		}
 	},
 	resolveLoader: {
@@ -24,23 +29,25 @@ module.exports = {
 		exprContextRegExp: /$^/,
 		exprContextCritical: false,
 		loaders: [
-			{ test: /src\/.*\.ts?$/, loader: 'ts-loader' }
+			{ test: /[src|test]\/.*\.ts?$/, loader: 'ts-loader' }
 		]
 	},
 	plugins: [
 		new CopyWebpackPlugin([
 			{ context: 'src', from: '**/*', ignore: '*.ts' },
+			{ context: 'test', from: '**/*', ignore: '*.ts' },
 		]),
 		new RequirePlugin(),
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}),
 		new HtmlWebpackPlugin ({
 			inject: true,
+			chunks: [ 'src/main' ],
 			template: 'src/index.html'
 		})
 	],
 	output: {
 		path: path.resolve('./dist'),
-		filename: 'main.js'
+		filename: '[name].js',
 	}
 };
