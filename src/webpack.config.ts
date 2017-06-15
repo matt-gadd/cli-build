dbfe4dbe13e1b2e03c047ba75eef6bbfc456b036
Matt Gadd <mgadd@sitepen.com>
2017-06-15 13:07:36 +0100

    add umd-compat-loader support for imports()

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -27,6 +27,33 @@ try {
 
 type IncludeCallback = (args: BuildArgs) => any;
 
+interface UMDCompatOptions {
+	bundles?: {
+		[key: string]: string[];
+	};
+}
+
+function getUMDCompatLoader(options: UMDCompatOptions) {
+	const { bundles = {} } = options;
+	return {
+		loader: 'umd-compat-loader',
+		options: {
+			imports(module: string, context: string) {
+				const filePath = path.relative(basePath, path.join(context, module));
+				let chunkName = filePath;
+				Object.keys(bundles).some((bundleName) => {
+					if (bundles[bundleName].indexOf(filePath) > -1) {
+						chunkName = bundleName;
+						return true;
+					}
+					return false;
+				});
+				return `promise-loader?global,${chunkName}!${module}`;
+			}
+		}
+	};
+}
+
 function webpackConfig(args: Partial<BuildArgs>) {
 	args = args || {};
 
@@ -246,7 +273,7 @@ function webpackConfig(args: Partial<BuildArgs>) {
 				{ test: /src[\\\/].*\.ts?$/, enforce: 'pre', loader: 'css-module-dts-loader?type=ts&instanceName=0_dojo' },
 				{ test: /src[\\\/].*\.m\.css?$/, enforce: 'pre', loader: 'css-module-dts-loader?type=css' },
 				{ test: /src[\\\/].*\.ts(x)?$/, use: [
-					'umd-compat-loader',
+					getUMDCompatLoader({ bundles: args.bundles }),
 					{
 						loader: 'ts-loader',
 						options: {
@@ -254,7 +281,7 @@ function webpackConfig(args: Partial<BuildArgs>) {
 						}
 					}
 				]},
-				{ test: /\.js?$/, loader: 'umd-compat-loader' },
+				{ test: /\.js?$/, use: [ getUMDCompatLoader({ bundles: args.bundles }) ] },
 				{ test: /globalize(\/|$)/, loader: 'imports-loader?define=>false' },
 				...includeWhen(!args.element, () => {
 					return [
@@ -268,7 +295,7 @@ function webpackConfig(args: Partial<BuildArgs>) {
 				...includeWhen(args.withTests, () => {
 					return [
 						{ test: /tests[\\\/].*\.ts?$/, use: [
-							'umd-compat-loader',
+							getUMDCompatLoader({ bundles: args.bundles }),
 							{
 								loader: 'ts-loader',
 								options: {
